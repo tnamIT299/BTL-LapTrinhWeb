44d2d0c775e5306664af36929465e57f01cdb863
tnamIT299 <trinhthanhnam2003@gmail.com>
2023-09-27 16:24:31 +0700

    update

--- client/Client_Home/Controllers/HomeController.cs
+++ client/Client_Home/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BTL_LapTrinhWeb_official_.Controllers
+namespace Client_Home.Controllers
 {
     public class HomeController : Controller
     {

f2aa09fba922b99928911a35b4c6d5c06c6dc38e
nguyenvanvi1 <nvvi.nvla1.2019@gmail.com>
2023-10-19 21:57:50 +0700

    addmodel

---
+++ client/Client_Home/Models/ProductComment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client_Home.Models;
+
+public partial class ProductComment
+{
+    public int CommentId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? CommentText { get; set; }
+
+    public byte[] CreatedDate { get; set; } = null!;
+
+    public virtual ICollection<CommentReply> CommentReplies { get; set; } = new List<CommentReply>();
+
+    public virtual Product? Product { get; set; }
+
+    public virtual User? User { get; set; }
+}
---
+++ client/Client_Home/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client_Home.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Salt { get; set; }
+
+    public string? Fullname { get; set; }
+
+    public int? Roleid { get; set; }
+
+    public DateTime? Lastlogin { get; set; }
+
+    public DateTime? Createdate { get; set; }
+
+    public bool? Active { get; set; }
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual ICollection<CommentReply> CommentReplies { get; set; } = new List<CommentReply>();
+
+    public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    public virtual ICollection<ProductComment> ProductComments { get; set; } = new List<ProductComment>();
+
+    public virtual Role? Role { get; set; }
+}

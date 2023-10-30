39ccfd48bcfbfd8b0c290a06e026b8bba5b1a12b
phuhuykoxl <phuhuy02003@gmail.com>
2023-10-30 23:06:54 +0700

    Update exportToexcel action in productController

--- client/Client_Home/Areas/Admin/Controllers/AdminProductsController.cs
+++ client/Client_Home/Areas/Admin/Controllers/AdminProductsController.cs
@@ -174,28 +174,49 @@ namespace Client_Home.Areas.Admin.Controllers
         {
           return (_context.Products?.Any(e => e.ProductId == id)).GetValueOrDefault();
         }
-        // GET: Admin/AdminCustomers/ExportToExcel
+
+
         public async Task<IActionResult> ExportToExcel()
         {
             using (var workbook = new XLWorkbook())
             {
                 var worksheet = workbook.Worksheets.Add("Products");
-                var currentRow = 1;
-                worksheet.Cell(currentRow, 1).Value = "ID";
-                worksheet.Cell(currentRow, 2).Value = "Tên sản phẩm";
-                worksheet.Cell(currentRow, 3).Value = "Mô tả";
-                worksheet.Cell(currentRow, 4).Value = "Giá bán";
-                worksheet.Cell(currentRow, 5).Value = "Danh mục";
-                worksheet.Cell(currentRow, 6).Value = "Hình ảnh";
-                worksheet.Cell(currentRow, 7).Value = "Bán chạy";
-                worksheet.Cell(currentRow, 8).Value = "Trạng thái";
-                worksheet.Cell(currentRow, 9).Value = "Ngày thêm";
-                worksheet.Cell(currentRow, 10).Value = "Nhà cung cấp";
+                // Set up font for worksheet
+                worksheet.Style.Font.FontName = "Times New Roman";
+                worksheet.Style.Font.FontSize = 13;
+                // Set up style for header row
+                var headerRow = worksheet.Row(1);
+                headerRow.Style.Font.Bold = true;
+                headerRow.Style.Font.FontSize = 14;
+                headerRow.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+
+                // Merge cells for table title
+                worksheet.Range("A1:J1").Merge();
+                worksheet.Cell(1, 1).Value = "Danh sách sản phẩm";
+                worksheet.Cell(1, 1).Style.Font.FontSize = 16;
+                worksheet.Cell(1, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                // Fill color 
+                worksheet.Range("A2:J2").Style.Fill.BackgroundColor = XLColor.Blue;
+                worksheet.Range("A2:J2").Style.Font.FontColor = XLColor.White;
+                worksheet.Range("A2:J2").Style.Font.Bold = true;
+                // Set up header row content
+                worksheet.Cell(2, 1).Value = "ID";
+                worksheet.Cell(2, 2).Value = "Tên sản phẩm";
+                worksheet.Cell(2, 3).Value = "Mô tả";
+                worksheet.Cell(2, 4).Value = "Giá bán";
+                worksheet.Cell(2, 5).Value = "Danh mục";
+                worksheet.Cell(2, 6).Value = "Hình ảnh";
+                worksheet.Cell(2, 7).Value = "Bán chạy";
+                worksheet.Cell(2, 8).Value = "Trạng thái";
+                worksheet.Cell(2, 9).Value = "Ngày thêm";
+                worksheet.Cell(2, 10).Value = "Nhà cung cấp";
+
+                var currentRow = 3;
+
                 if (_context.Products != null)
                 {
                     foreach (var product in await _context.Products.Include(p => p.Category).Include(p => p.Supplier).ToListAsync())
                     {
-                        currentRow++;
                         worksheet.Cell(currentRow, 1).Value = product.ProductId;
                         worksheet.Cell(currentRow, 2).Value = product.Name;
                         worksheet.Cell(currentRow, 3).Value = product.Description;
@@ -206,14 +227,19 @@ namespace Client_Home.Areas.Admin.Controllers
                         worksheet.Cell(currentRow, 8).Value = (XLCellValue)product.Active;
                         worksheet.Cell(currentRow, 9).Value = (XLCellValue)product.DateAdded;
                         worksheet.Cell(currentRow, 10).Value = product.Supplier.SupplierName;
+
+                        currentRow++;
                     }
                 }
+                for (int i = 1; i <= 10; i++)
+                {
+                    worksheet.Column(i).AdjustToContents();
+                }       
 
                 using (var stream = new MemoryStream())
                 {
                     workbook.SaveAs(stream);
                     var content = stream.ToArray();
-                    //_notifyService.Success("Đã xuất file thành công");
                     return File(
                         content,
                         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
@@ -221,5 +247,6 @@ namespace Client_Home.Areas.Admin.Controllers
                 }
             }
         }
+
     }
 }

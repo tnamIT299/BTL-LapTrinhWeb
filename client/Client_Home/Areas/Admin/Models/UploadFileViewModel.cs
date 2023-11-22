094a73397cfcb1a4417bdcd0541817e5c7400980
tnamIT299 <trinhthanhnam2003@gmail.com>
2023-11-22 14:15:25 +0700

    merge

---
+++ client/Client_Home/Areas/Admin/Models/UploadFileViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client_Home.Areas.Admin.Models
+{
+    public class UploadFileViewModel
+    {
+        [Required(ErrorMessage = "Vui lòng chọn một tệp Excel.")]
+        [DataType(DataType.Upload)]
+        public IFormFile File { get; set; }
+    }
+}

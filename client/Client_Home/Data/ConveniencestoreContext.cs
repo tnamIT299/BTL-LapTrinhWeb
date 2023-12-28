87a388f4e4b55a0b39e2fcb7338db9cbc77783dd
tnamIT299 <trinhthanhnam2003@gmail.com>
2023-12-29 00:06:43 +0700

    fix

--- client/Client_Home/Data/ConveniencestoreContext.cs
+++ client/Client_Home/Data/ConveniencestoreContext.cs
@@ -43,7 +43,7 @@ public partial class ConveniencestoreContext : DbContext
 
     public virtual DbSet<InvoiceDetail> InvoiceDetails { get; set; }
 
-    public virtual DbSet<Orders> Orders { get; set; }
+    public virtual DbSet<Order> Orders { get; set; }
 
     public virtual DbSet<OrderDetail> OrderDetails { get; set; }
 
@@ -351,6 +351,9 @@ public partial class ConveniencestoreContext : DbContext
             entity.Property(e => e.InvoiceId).HasColumnName("InvoiceID");
             entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
             entity.Property(e => e.ProductBatchId).HasColumnName("productBatchID");
+            entity.Property(e => e.UnitPrice)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("unitPrice");
 
             entity.HasOne(d => d.Invoice).WithMany(p => p.InvoiceDetails)
                 .HasForeignKey(d => d.InvoiceId)
@@ -361,7 +364,7 @@ public partial class ConveniencestoreContext : DbContext
                 .HasConstraintName("FK__InvoiceDe__produ__662B2B3B");
         });
 
-        modelBuilder.Entity<Orders>(entity =>
+        modelBuilder.Entity<Order>(entity =>
         {
             entity.HasKey(e => e.OrderId).HasName("PK__Orders__C3905BAFD0BC4AE7");
 
@@ -400,6 +403,22 @@ public partial class ConveniencestoreContext : DbContext
                 .HasForeignKey(d => d.ProductId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__OrderDeta__Produ__681373AD");
+        });
+
+        modelBuilder.Entity<Pannel>(entity =>
+        {
+            entity.HasKey(e => e.IdPannel);
+
+            entity.ToTable("Pannel");
+
+            entity.Property(e => e.IdPannel).HasColumnName("Id_pannel");
+            entity.Property(e => e.NamePannel)
+                .HasMaxLength(150)
+                .HasColumnName("name_pannel");
+            entity.Property(e => e.UrlPannel)
+                .HasMaxLength(255)
+                .IsFixedLength()
+                .HasColumnName("url_pannel");
         });
 
         modelBuilder.Entity<Payment>(entity =>
